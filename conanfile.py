302ca35a403a5b49b3d2fdef5d335f47ecb01aef
Steven Ewald <stevenewald6@gmail.com>
2025-04-15 15:45:12 -0500

    Add GLFW, GLAD, and ImGui

--- conanfile.py
+++ conanfile.py
@@ -10,6 +10,9 @@ class Recipe(ConanFile):
 
     def requirements(self):
         self.requires("fmt/11.0.2")
+        self.requires("glfw/3.4")
+        self.requires("imgui/1.91.8")
+        self.requires("glad/0.1.36")
 
     def build_requirements(self):
         self.test_requires("catch2/3.7.0")
