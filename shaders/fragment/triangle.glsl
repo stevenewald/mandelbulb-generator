af7b193de7e6472c50ba62e94954a0711ac05569
Steven Ewald <stevenewald6@gmail.com>
2025-04-20 15:38:01 -0500

    Added colors

--- shaders/fragment/triangle.glsl
+++ shaders/fragment/triangle.glsl
@@ -10,6 +10,20 @@ uniform float iTime;
 #define EPSILON 0.0001
 #define Power 3
 
+vec3 palette(float t, vec3 a, vec3 b, vec3 c, vec3 d){
+    return a + b * cos(6.28318 * (c * t + d));
+}
+
+vec4 colorForQ(int steps){
+    float t = clamp(float(steps) / 20, 0.0, 1.0);
+    // tweak these to taste:
+    vec3 A = vec3(0.8, .5, .4);
+    vec3 B = vec3(0.2, .4, .2);
+    vec3 C = vec3(2.0, 1.0, 1.0);
+    vec3 D = vec3(0, .25, .25);
+    return vec4(palette(t, A, B, C, D), 1.0);
+}
+
 float SDF(vec3 pos, out int steps) {
 	vec3 z = pos;
 	float dr = 1.0;
@@ -56,14 +70,14 @@ float trace(vec3 ro, vec3 rd, out int steps) {
 void main()
 {
 	vec2 xy = pos - iResolution.xy / 2.0;
-	vec3 ro = vec3(0.0, 0.5 * sin(iTime), 2.0 + sin(iTime / 3.0));
+	vec3 ro = vec3(0.25 * cos(iTime), 0.5 * sin(iTime), 2.0 + .8 * sin(iTime / 3.0));
 	vec3 rd = normalize(vec3(xy, -iResolution.y / tan(radians(50.0) / 2.0)));
 	int steps = 0;
 
 	float dist = trace(ro, rd, steps);
 
 	if (dist < MAX_DIST) {
-		fragColor = vec4(vec3(clamp(float(steps) / 20.0, 0.0, 1.0)), 1.0);
+		fragColor = colorForQ(steps);
 		return;
 	}
 
--- source/shader.cpp
+++ source/shader.cpp
@@ -6,7 +6,6 @@
 #include <cstdint>
 
 #include <array>
-#include <expected>
 #include <filesystem>
 #include <fstream>
 #include <stdexcept>
